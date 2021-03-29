ee311d927f1b60890cd8ca920bc22a0653d51825
leonardo <leonardolacerda.as@gmail.com>
2021-03-29 20:24:58 -0300

    Criando as classes de Produto e Cliente. Vinculando estas classes a classe Venda.

--- vendas/models.py
+++ vendas/models.py
@@ -16,8 +16,26 @@ class Venda(models.Model):
     email_cliente = models.EmailField(blank=True, null=True)
     venda_concluida = models.BooleanField(blank=False, null=False)
     qtd_itens = models.IntegerField(blank=True, null=False, default=0)
+    produtos = models.ManyToManyField('Produto')
+    cliente = models.ForeignKey('Cliente', on_delete=models.DO_NOTHING, default=1)
 
     def __str__(self):
         return str(self.pk) + ' - ' + self.nome
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    valor = models.DecimalField(max_digits=6, decimal_places=2, blank=False, null=False)
+
+    def __str__(self):
+        return self.nome + '. R$: ' + str(self.valor)
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    cpf = models.CharField(max_length=11, blank=False, null=False)
+
+    def __str__(self):
+        return self.nome
 
 
