c599e35095c5220ac5a6267a6249776dc1c2d393
leonardo <leonardolacerda.as@gmail.com>
2021-04-05 20:49:29 -0300

    Inclusão da taf forms, do botão de submit e do CSRF_TOKEN para validação no submit

--- vendas/views.py
+++ vendas/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 from .models import Venda
-
+from django.urls import reverse_lazy
 # Create your views here.
 
 class VendaCreateView(CreateView):
@@ -9,3 +9,7 @@ class VendaCreateView(CreateView):
     template_name = 'cadastrar/venda.html'
 
     fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy("cadastrar_venda")
+
