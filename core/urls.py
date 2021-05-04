e5262ce7d3fbbbfa3f1e8815574f33e7c262ce47
leonardo <leonardolacerda.as@gmail.com>
2021-05-03 21:14:07 -0300

    Configuração de ambiente para conexão com o banco de dados PostgreSQL com "Psycopg2" e configuração no urls.py para exibição de imagens

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from vendas import urls as urls_vendas
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vendas/', include(urls_vendas)),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+              + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
